6431a6d96e6989c4e25e3a3766b3960d43c20705
DalhaMohamed <119519765+DalhaMohamed@users.noreply.github.com>
2022-12-22 11:48:05 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,20 @@
+# this is my first code 
+
+#x = 5 
+#print(x)  
+
+#x = 6
+#y = 229383 
+
+#print(x + y) 
+
+#x = 10
+#y = "dalha"
+
+#print(x , y) 
+
+x = "dalha"
+y = "mohamed" 
+
+print(x , y) 
+
